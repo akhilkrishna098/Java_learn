528fee4fbe8431ba861d757753dcbf42ea5e4440
akhil_krishna <aakhilkrishna098@gmail.com>
2024-02-02 10:39:42 +0100

    DaimondPattern

--- PatternPrint/Star.java
+++ PatternPrint/Star.java
@@ -10,16 +10,36 @@ public class Star {
 //        }
 //    }
 
+    //Right Angled Triangle
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 5; i++) {
+//           for (int j = 1; j <= i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println(" ");
+//        }
+//    }
+    //Pyramid
     public static void main(String[] args) {
-        for(int i = 1 ; i <= 3; i++){
-            for(int j=1 ; j < i+1; j++){
-                for( int n = 3 ; n > 0 ; n--){
-                    System.out.print("\t");
-                }
-                System.out.print("*");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
             }
-            System.out.println("");
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+        for (int a = 4; a >= 1; a--) {
+            for (int b = a; b <= 4; b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= a; c++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
 
